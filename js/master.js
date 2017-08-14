4f5a8d3ae44e26138995f9c42abaec1c23341ba4
sanzillion <sanzimagery@gmail.com>
2017-08-14 15:16:33 +0800

    for checking purpose

--- js/master.js
+++ js/master.js
@@ -1,25 +1,5 @@
 // Vanilla
 
-	var tempanswer;
-	var easy = [
-	{
-		question: "Who are you?",
-		choices: ["you", "me"
-		, "who"],
-		answer: "you"
-	},
-	{
-		question: "Where you live?",
-		choices: ["here", "there", "everywhere"],
-		answer: "here"
-	},
-	{
-		question: "What you do?",
-		choices: ["this", "that", "fuck"],
-		answer: "fuck"
-	}
-	];
-
 var index = 0;
 var answer;
 
@@ -74,7 +54,9 @@ $(document).ready(function(){
 
 	var inhead = $('.inner-head');
 	var con = $('.content');
-	$('.inner-content').css("opacity", "0");
+	var incon = $('.inner-content');
+	incon.css("opacity", "0");
+	incon.hide();
 	inhead.css("opacity", "0");
 	con.css("height", "150px")
 
@@ -83,7 +65,8 @@ $(document).ready(function(){
 		con.animate({height: "370px"})
 		inhead.animate({opacity: "1"});
 		inhead.text("Choose difficulty:")
-		$('.inner-content').animate({opacity: "1"});
+		incon.show();
+		incon.animate({opacity: "1"});
 	});
 
 		var ch = $("#ch");
